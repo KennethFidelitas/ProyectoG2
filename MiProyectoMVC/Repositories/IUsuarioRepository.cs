9e91fc28cb821644ff370c20e1533d7e9b30f9a5
KennethFidelitas <kgomez90692@ufide.ac.cr>
2026-04-21 16:45:45 -0600

    faltante

---
+++ MiProyectoMVC/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using MiProyectoMVC.Models;
+
+namespace MiProyectoMVC.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        List<Usuario> GetAll();
+        Usuario ObtenerPorId(int id);
+        Usuario ObtenerPorIdentificacion(string identificacion);
+        void Agregar(Usuario usuario);
+        void Actualizar(Usuario usuario);
+    }
+}
---
+++ MiProyectoMVC/Repositories/UsuarioRepository.cs
@@ -0,0 +1,42 @@
+﻿using MiProyectoMVC.Models;
+
+namespace MiProyectoMVC.Repositories
+{
+    public class UsuarioRepository
+    {
+        private readonly TuDbContext _context;
+
+        public UsuarioRepository(TuDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Usuario> GetAll()
+        {
+            return _context.Usuarios.ToList();
+        }
+
+        public Usuario GetById(int id)
+        {
+            return _context.Usuarios.Find(id);
+        }
+
+        public Usuario GetByIdentificacion(string identificacion)
+        {
+            return _context.Usuarios
+                .FirstOrDefault(u => u.Identificacion == identificacion);
+        }
+
+        public void Add(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+        }
+
+        public void Update(Usuario usuario)
+        {
+            _context.Usuarios.Update(usuario);
+            _context.SaveChanges();
+        }
+    }
+}
